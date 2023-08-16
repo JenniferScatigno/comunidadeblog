2eb5ac2ad76d1f9b0f2b96316f0a5b61cf5179dc
JenniferScatigno <107508502+JenniferScatigno@users.noreply.github.com>
2023-08-16 14:55:26 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bc791cc0f3019a2c976d81673700b245'
 if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URL'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///comunidade.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -18,4 +19,14 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login_criarconta'
 login_manager.login_message_category = 'alert-info'
 
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('base de dados criado')
+else:
+    print('banco de dados já existente')
 from comunidadeimpressionadora import routes
